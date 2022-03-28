fbfac803a666cf65ebf4ea24c33c79a2eff88737
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-03-28 10:13:13 +0200

    Simplify the algorithm for computing reentry nodes during external transitions

--- packages/core/src/StateNode.ts
+++ packages/core/src/StateNode.ts
@@ -958,11 +958,7 @@ class StateNode<
 
     if (!isInternal) {
       allNextStateNodes.forEach((n1) => {
-        this.nodesFromChild(n1).forEach((n2) => {
-          if (n2 !== this.machine) {
-            reentryNodes.push(n2);
-          }
-        });
+        reentryNodes.push(...this.getExternalReentryNodes(n1));
       });
     }
 
@@ -976,13 +972,9 @@ class StateNode<
     };
   }
 
-  private nodesFromChild(
+  private getExternalReentryNodes(
     childStateNode: StateNode<TContext, any, TEvent, any, any, any>
   ): Array<StateNode<TContext, any, TEvent, any, any, any>> {
-    if (childStateNode.escapes(this)) {
-      return [];
-    }
-
     const nodes: Array<StateNode<TContext, any, TEvent, any, any, any>> = [];
     let marker:
       | StateNode<TContext, any, TEvent, any, any>
@@ -992,32 +984,16 @@ class StateNode<
       nodes.push(marker);
       marker = marker.parent;
     }
-    nodes.push(this); // inclusive
-
+    if (marker !== this) {
+      // we never got to `this`, therefore the `childStateNode` "escapes" it
+      // it's in a different part of the tree so no states will be reentered for such an external transition
+      return [];
+    }
+    // root is never exited, so it also can't be reentered
+    if (marker.parent) {
+      nodes.push(this);
+    }
     return nodes;
-  }
-
-  /**
-   * Whether the given state node "escapes" this state node. If the `stateNode` is equal to or the parent of
-   * this state node, it does not escape.
-   */
-  private escapes(
-    stateNode: StateNode<TContext, any, TEvent, any, any, any>
-  ): boolean {
-    if (this === stateNode) {
-      return false;
-    }
-
-    let parent = this.parent;
-
-    while (parent) {
-      if (parent === stateNode) {
-        return false;
-      }
-      parent = parent.parent;
-    }
-
-    return true;
   }
 
   private getActions(
